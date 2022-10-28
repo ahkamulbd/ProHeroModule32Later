e30501359687b9c0e79897af27d444f896db8031
ahkamulbd <ahkamulbd@gmail.com>
2022-10-28 16:34:58 +0600

    Module 32 initiated and Module 32-2 done

---
+++ json.js
@@ -0,0 +1,27 @@
+const user = { id: 1, name: 'Gorib Amir', job: 'actor' };
+// JavaScript Object Notation(JSON)
+const stringified = JSON.stringify(user);
+
+//console.log(user);
+//console.log(stringified);
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'Wapda Road',
+        city: 'chakaria',
+        country: 'BD'
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false
+}
+
+//console.log(shop);
+
+const shopJSON = JSON.stringify(shop);
+//console.log(shopJSON);
+
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
